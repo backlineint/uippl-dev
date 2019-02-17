1b8c96c588a4b43c3cb0cc28c60f4a05e54eb606
Brian Perry <contact.brianperry@gmail.com>
2019-02-17 16:18:47 -0500

    Minor update to install script

--- clean-install.sh
+++ clean-install.sh
@@ -8,6 +8,10 @@ composer install
 
 lando drush si standard -y --db-url='mysql://drupal8:drupal8@database/drupal8' --site-name='UIPPL Dev' --account-name=admin --account-pass=admin
 
+# Latest Tagged Release
+# composer require 'drupal/ui_patterns_pattern_lab'
+
+# Dev
 composer require 'drupal/ui_patterns_pattern_lab:1.x-dev'
 
 lando drush en -y components ui_patterns_pattern_lab components
